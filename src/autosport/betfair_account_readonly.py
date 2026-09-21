c143138b9e9c6097daab5de975d886303ae2960c
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-21 23:52:48 +0200

    [PROVIDER] Fail closed on conflicting Betfair exception identity

--- src/autosport/betfair_account_readonly.py
+++ src/autosport/betfair_account_readonly.py
@@ -914,7 +914,11 @@ def _provider_error_code(data: object) -> str | None:
     present = [key for key in exception_keys if key in data]
     if len(present) != 1:
         return None
-    exception = data[present[0]]
+    exception_key = present[0]
+    declared_exception = data.get("exceptionname")
+    if declared_exception is not None and declared_exception != exception_key:
+        return None
+    exception = data[exception_key]
     if not isinstance(exception, Mapping):
         return None
     code = exception.get("errorCode")

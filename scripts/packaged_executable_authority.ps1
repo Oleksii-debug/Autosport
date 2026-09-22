9b065bf34c2d504495cd91afa4467bf3323f684b
Oleksii-debug <oleksiy.az.09@gmail.com>
2026-09-22 23:27:06 +0200

    Enforce whole-product false truth in producer authority

--- scripts/packaged_executable_authority.ps1
+++ scripts/packaged_executable_authority.ps1
@@ -279,7 +279,8 @@ function Get-AutosportProducerPackageIdentity {
     if ($buildInfo.real_money_execution -ne $false -or
         $buildInfo.human_tested -ne $false -or
         $buildInfo.nvda_verified -ne $false -or
-        $buildInfo.v1_ready -ne $false) {
+        $buildInfo.v1_ready -ne $false -or
+        $buildInfo.whole_product_complete -ne $false) {
       throw 'Producer-bound BUILD_INFO violated prehuman truth labels'
     }
 
